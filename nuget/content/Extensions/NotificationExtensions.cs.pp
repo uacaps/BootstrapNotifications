bd4985ae404ca4c87db0036e262e39d834b08abb
Carrie Kendall <carrie.a.kendall@gmail.com>
2014-01-15 15:47:39 -0600

    restructuring the project
    organizing the project, and adding nuget for easier distribution

---
+++ nuget/content/Extensions/NotificationExtensions.cs.pp
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace $rootnamespace$.Extensions
+{
+    /*                                                                      *
+     *      This extension was derive from Brad Christie's answer           *
+     *      on StackOverflow.                                               *
+     *                                                                      *
+     *      The original code can be found at:                              *
+     *      http://stackoverflow.com/a/18338264/998328                      *
+     *                                                                      */
+
+    public static class NotificationExtensions
+    {
+        private static IDictionary<String, String> NotificationKey = new Dictionary<String, String>
+        {
+            { "Error",      "App.Notifications.Error" }, 
+            { "Warning",    "App.Notifications.Warning" },
+            { "Success",    "App.Notifications.Success" },
+            { "Info",       "App.Notifications.Info" }
+        };
+
+
+        public static void AddNotification(this ControllerBase controller, String message, String notificationType)
+        {
+            string NotificationKey = getNotificationKeyByType(notificationType);
+            ICollection<String> messages = controller.TempData[NotificationKey] as ICollection<String>;
+
+            if (messages == null)
+            {
+                controller.TempData[NotificationKey] = (messages = new HashSet<String>());
+            }
+
+            messages.Add(message);
+        }
+
+        public static IEnumerable<String> GetNotifications(this HtmlHelper htmlHelper, String notificationType)
+        {
+            string NotificationKey = getNotificationKeyByType(notificationType);
+            return htmlHelper.ViewContext.Controller.TempData[NotificationKey] as ICollection<String> ?? null;
+        }
+
+        private static string getNotificationKeyByType(string notificationType)
+        {
+            try
+            {
+                return NotificationKey[notificationType];
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                ArgumentException exception = new ArgumentException("Key is invalid", "notificationType", e);
+                throw exception;
+            }
+        }
+    }
+
+    public static class NotificationType
+    {
+        public const string ERROR = "Error";
+        public const string WARNING = "Warning";
+        public const string SUCCESS = "Success";
+        public const string INFO = "Info";
+
+    }
+}
---
+++ nuget/content/Views/Shared/_Notifications.cshtml.pp
@@ -0,0 +1,92 @@
+﻿@using $rootnamespace$.Extensions
+
+@{
+    var errorList = Html.GetNotifications(NotificationType.ERROR);
+    var warningList = Html.GetNotifications(NotificationType.WARNING);
+    var successList = Html.GetNotifications(NotificationType.SUCCESS);
+    var infoList = Html.GetNotifications(NotificationType.INFO);
+}
+<!-- display errors -->
+@if (errorList != null)
+{
+    <div class="alert alert-danger alert-dismissable">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+        @if(errorList.Count() > 1){
+            <strong><span class="glyphicon glyphicon-remove"></span> There are @errorList.Count() errors: </strong>
+            <ul>
+                @foreach (String message in errorList)
+                {
+                    <li>@Html.Raw(message)</li>
+                }
+            </ul>
+        }
+        else{
+            <strong><span class="glyphicon glyphicon-remove"></span> Error: </strong>
+            @Html.Raw(errorList.First())
+        }
+    </div>
+}
+
+<!-- display warnings -->
+@if (warningList != null)
+{
+    <div class="alert alert-warning alert-dismissable">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+        @if(warningList.Count() > 1){
+            <strong><span class="glyphicon glyphicon-warning-sign"></span> There are @warningList.Count() warnings: </strong>
+            <ul>
+                @foreach (String message in warningList)
+                {
+                    <li>@Html.Raw(message)</li>
+                }
+            </ul>
+        }
+        else{
+            <strong><span class="glyphicon glyphicon-warning-sign"></span> Warning: </strong>
+            @Html.Raw(warningList.First())
+        }
+    </div>
+}
+
+<!-- display success -->
+@if (successList != null)
+{
+    <div class="alert alert-success alert-dismissable">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+        @if(successList.Count() > 1){
+            <strong><span class="glyphicon glyphicon-ok"></span> There are @successList.Count() successful notifications: </strong>
+            <ul>
+                @foreach (String message in successList)
+                {
+                    <li>@Html.Raw(message)</li>
+                }
+            </ul>
+        }
+        else{
+            <strong><span class="glyphicon glyphicon-ok"></span> Success! </strong>
+            @Html.Raw(successList.First())
+        }
+    </div>
+}
+
+<!-- display success -->
+@if (infoList != null)
+{
+    <div class="alert alert-info alert-dismissable">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+        @if(infoList.Count() > 1){
+            <strong><span class="glyphicon glyphicon-info-sign"></span> There are @infoList.Count() notifications: </strong>
+            <ul>
+                @foreach (String message in infoList)
+                {
+                    <li>@Html.Raw(message)</li>
+                }
+            </ul>
+        }
+        else{
+            <strong><span class="glyphicon glyphicon-info-sign"></span> </strong>
+            @Html.Raw(infoList.First())
+        }
+    </div>
+}
+
